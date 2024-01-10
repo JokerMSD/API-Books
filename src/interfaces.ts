bdec1a2395b32c8550bae62c5beae87611b15a98
IgorKenzie <135076464+JokerMSD@users.noreply.github.com>
2024-01-10 14:17:25 -0300

    update interfaces.ts

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,8 +1,34 @@
-export interface Object {
-    id: number;
-    name: string;
-    pages: number;
-    category: string;
-    createdAt: Date;
-    updatedAt: Date;
+import { Request, Response, NextFunction } from "express";
+
+export class MyObject {
+  id: number;
+  name: string;
+  pages: number;
+  category: string;
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor(
+    id: number,
+    name: string,
+    pages: number,
+    category: string,
+    createdAt: Date,
+    updatedAt: Date,
+  ) {
+    this.id = id;
+    this.name = name;
+    this.pages = pages;
+    this.category = category;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+}
+
+export interface ServiceInterface {
+  execute(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): void | Response<any, Record<string, any>>;
 }
