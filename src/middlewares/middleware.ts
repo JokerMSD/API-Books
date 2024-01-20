9017bdb049e9fbfb110a7c35bfefde192332efd1
JokerMSD <igor332004@gmail.com>
2024-01-20 15:48:09 -0300

    update middleware.ts

--- src/middlewares/middleware.ts
+++ src/middlewares/middleware.ts
@@ -1,30 +1,43 @@
 import { Request, Response, NextFunction } from "express";
 import { booksDatabase } from "../database/database";
 import { ServiceInterface } from "../interfaces/interfaces";
-
+import { AppError } from "../errors/AppError";
+import { AnyZodObject, Schema, ZodError } from "zod";
 
 export class Service implements ServiceInterface {
   execute(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+    req: Request | undefined,
+    res: Response | undefined,
+    next: NextFunction | undefined,
   ): void | Response<any, Record<string, any>> {
     throw new Error("Method not implemented.");
   }
 }
 
-export class errorHandler extends Service {
-  static execute(err: Error, req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>>, next: NextFunction) {
-    throw new Error("Method not implemented.");
-  }
-  execute(
+export class GlobalErrors {
+  handleErrors = (
+    err: Error,
     req: Request,
     res: Response,
     next: NextFunction,
-  ): void | Response<any, Record<string, any>> {
-    console.error(req, res, next);
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
+  ): Response => {
+    if (err instanceof AppError) {
+      return res.status(err.statusCode).json({ error: err.message });
+    }
+
+    if (err instanceof ZodError) {
+      return res.status(409).json(err);
+    }
+
+    return res.status(500).json({ error: "Internal server error" });
+  };
+
+  validateBody = (schema: AnyZodObject) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        req.body = await schema.parseAsync(req.body);
+        return next();
+    };
+  };
 }
 
 export class CheckDuplicateBookName extends Service {
@@ -37,7 +50,7 @@ export class CheckDuplicateBookName extends Service {
     const existingBook = booksDatabase.find((book) => book.name === bookName);
 
     if (existingBook) {
-      return res.status(409).json({ error: "Book already registered." });
+      throw new AppError(409, "Book already registered.");
     }
 
     return next();
@@ -58,7 +71,7 @@ export class CheckBookExistence extends Service {
     const bookExists = booksDatabase.some((book) => book.id === id);
 
     if (!bookExists) {
-      return res.status(404).json({ error: "Book not found." });
+      throw new AppError(404, "Book not found.");
     }
 
     return next();
