06945ad96d4cfb1f7ac73031ff3e87cdb2ba6aac
JokerMSD <igor332004@gmail.com>
2024-01-20 15:48:12 -0300

    update routes.ts

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -3,24 +3,27 @@ import { Router } from "express";
 import {
   CheckBookExistence,
   CheckDuplicateBookName,
+  GlobalErrors,
 } from "../middlewares/middleware";
+import { bookCreateSchema, bookUpdateSchema } from "../schemas/books.schemas";
 
 const router = Router();
+const globalErrors = new GlobalErrors();
 
-router.use("/books", CheckDuplicateBookName.getInstance().execute)
+router.use("/books", CheckDuplicateBookName.getInstance().execute);
 
-router.use("/books/:id", CheckBookExistence.getInstance().execute)
+router.use("/books/:id", CheckBookExistence.getInstance().execute);
 
 
 
-router.get("/", BookController.getAllBooks);
+router.post("/books", globalErrors.validateBody(bookCreateSchema), BookController.createBook);
+
+router.get('/books', BookController.getBooks);
 
 router.get("/books/:id", BookController.getBookById);
 
-router.post("/books", BookController.createBook,);
+router.patch("/books/:id",globalErrors.validateBody(bookUpdateSchema), BookController.updateBook);
 
-router.patch("/books/:id", BookController.updateBook,);
-
-router.delete("/books/:id", BookController.deleteBook,);
+router.delete("/books/:id", BookController.deleteBook);
 
 export default router;
